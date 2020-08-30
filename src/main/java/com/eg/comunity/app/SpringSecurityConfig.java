5a4bcdf8f7f945ead419a5ea3621705e62a46f60
rgarcial1983 <rafa.garcia.leon@gmail.com>
2020-08-30 16:04:12 +0200

    selector activo según opción

--- src/main/java/com/eg/comunity/app/SpringSecurityConfig.java
+++ src/main/java/com/eg/comunity/app/SpringSecurityConfig.java
@@ -28,7 +28,7 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 
 		http.authorizeRequests()
-				.antMatchers("/", "/css/**", "/js/**", "/images/**", "/font/**", "/uploads/**", "/webfonts/**", "/usuario/create", "/torneo/listar", "/evento/listar", "/recuperarPass",
+				.antMatchers("/", "/css/**", "/js/**", "/images/**", "/font/**","/icons/**", "/uploads/**", "/webfonts/**", "/usuario/create", "/torneo/listar", "/evento/listar", "/recuperarPass",
 						"/configuracion")
 				.permitAll()
 				/* .antMatchers("/ver/**").hasAnyRole("USER") */
